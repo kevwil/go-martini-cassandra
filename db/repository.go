48cdce7f503546be1d57f1e5c1efa907d2f44462
Kevin Williams <kevwil@gmail.com>
2015-01-15 18:49:09 -0500

    get templates to render - display works now

--- db/repository.go
+++ db/repository.go
@@ -40,15 +40,15 @@ func (r *Repository) Finish() {
 	r.Sess.Close()
 }
 
-func (r *Repository) GetAllPosts() []*models.Post {
-	var posts []*models.Post
+func (r *Repository) GetAllPosts() []models.Post {
+	var posts []models.Post
 	var id uint
 	var title, tags, content string
 	var date time.Time
 	iter := r.Sess.Query(`SELECT id,title,tags,content,date FROM posts`).Iter()
 	for iter.Scan(&id, &title, &tags, &content, &date) {
 		newPost := models.Post{Id: id, Title: html.EscapeString(title), Tags: tags, Content: content, Date: date}
-		posts = append(posts, &newPost)
+		posts = append(posts, newPost)
 	}
 	if err := iter.Close(); err != nil {
 		panic(err)
@@ -64,6 +64,5 @@ func (r *Repository) GetSinglePost(title string) models.Post {
 	if err != nil {
 		panic(err)
 	}
-	newPost := models.Post{Id: id, Title: html.EscapeString(mytitle), Tags: tags, Content: content, Date: date}
-	return newPost
+	return models.Post{Id: id, Title: html.EscapeString(mytitle), Tags: tags, Content: content, Date: date}
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/go-martini/martini"
+	"github.com/martini-contrib/render"
 	"github.com/kevwil/go-martini-cassandra/db"
 	"github.com/kevwil/go-martini-cassandra/models"
 )
@@ -9,27 +10,45 @@ import (
 var repo db.Repository
 
 func main() {
-	repo := db.NewRepository("127.0.0.1", "mykeyspace")
+	repo = *db.NewRepository("127.0.0.1", "mykeyspace")
 	repo.Begin()
 	defer repo.Finish()
 
-	mc := martini.Classic()
-	// mc.Get("/", GetHome)
-	// mc.Get("/favicon.ico", func() (int,string) {
-	//     return 404, "Not Found"
-	// })
-	mc.Get("/blog", GetBlogListing)
-	mc.Get("/blog/:title", GetBlogSingle)
+	m := martini.Classic()
+	m.Use(render.Renderer(render.Options{
+		Extensions: []string{".html"},
+	}))
+	m.Get("/", GetHome)
+	m.Get("/favicon.ico", func() (int,string) {
+		return 404, "Not Found"
+	})
+	m.Get("/blog", GetBlogListing)
+	m.Get("/blog/:title", GetBlogSingle)
 
-	mc.Run()
+	m.Run()
+}
+
+// GetHome = render home page
+func GetHome(r render.Render) {
+	r.HTML(200, "index", map[string]interface{}{"PageTitle": "Home"})
+}
+
+type Listing struct {
+	PageTitle string
+	Posts []models.Post
 }
 
 // GetBlogListing = retrieve list of blog posts
-func GetBlogListing() []*models.Post {
-	return repo.GetAllPosts()
+func GetBlogListing(r render.Render) {
+	r.HTML(200, "listing", Listing{"Blog",repo.GetAllPosts()})
+}
+
+type PostOutput struct {
+	PageTitle string
+	Post models.Post
 }
 
 // GetBlogSingle = get a single blog post by title
-func GetBlogSingle(params martini.Params) models.Post {
-	return repo.GetSinglePost(params["title"])
+func GetBlogSingle(params martini.Params, r render.Render) {
+	r.HTML(200, "single", PostOutput{params["title"],repo.GetSinglePost(params["title"])})
 }
